073ed02c36f86106177612c7e0d3d355296380dd
sonofkoolaidman <35243264+sonofkoolaidman@users.noreply.github.com>
2019-02-19 09:04:33 -0600

    trying to get guesscount to work

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,10 +1,28 @@
-
+import java.util.Random;
+import java.util.Scanner;
 public class GuessingGame {
 
     public static void main(String[] args) {
     
-        System.out.println("Welcome to the guessing game.");
-    
+      System.out.println("Welcome to the guessing game.");
+      
+      Random rand = new Random();
+      int rnumber = rand.nextInt(101);
+      int guesscount = 0;
+      Scanner myObj = new Scanner(System.in);
+      int input = myObj.nextInt();
+      while (input != rnumber) {
+        guesscount = guesscount++;
+        if (input < rnumber) {
+          System.out.println("too low");
+          input = myObj.nextInt();
+        } else if (input > rnumber) {
+          System.out.println("too high"); 
+          input = myObj.nextInt();
+        }
+      }
+      System.out.println("You guessed correctly! it took you " + guesscount + " tries");
+      
     }
-
+    
 }
